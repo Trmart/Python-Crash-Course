febb02866a357e9d5067a9395c310e596870278e
Taylor <trmartin2@nic.edu>
2021-10-27 17:33:46 -0700

    Updated Ch9 code

--- Ch9_classes/9-6_ice_cream_stand.py
+++ Ch9_classes/9-6_ice_cream_stand.py
@@ -4,3 +4,42 @@
 # the class will work; just pick the one you like better. Add an attribute called 
 # flavors that stores a list of ice cream flavors. Write a method that displays 
 # these flavors. Create an instance of IceCreamStand, and call this method.
+
+class Restaurant():
+    """A Simple class example in Python to model restaurant info"""
+    # __init__() is similar to a class constructor in C++ 
+    def __init__(self,restaurant_name, cuisine_type):  
+        """Initialize name and cuisine attributes"""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        """Prints the two pieces of info about the restaurant sent into it"""
+        print(str(self.restaurant_name).title() + ' Specializes In '  + str(self.cuisine_type).title() + ' Cuisine')
+
+    def open_restaurant(self):
+        """prints a message indicating the restaurant is open"""
+        print(str(self.restaurant_name).title() + " is open and ready for business!".title())
+
+class IceCreamStand(Restaurant):
+    """A specific restuarant subclass IceCreamStand that inherits from the superclass Restuarant"""
+    def __init__(self,restaurant_name,cuisine_type) -> None:
+        """Uses super() to inherit __init__() methods from the parent(super) class"""
+        super().__init__(restaurant_name,cuisine_type)
+    
+    flavors = ['vanilla','chocolate','strawberry','poop']
+
+    def print_flavors_list(self):
+        """this function prints a list of flavors"""
+        for flavor in self.flavors:
+            print(str(flavor).title())
+def main():
+    """main function"""
+    cold_stone = IceCreamStand('cold stone','ice cream')
+    cold_stone.open_restaurant()
+    cold_stone.describe_restaurant()
+    print(str(cold_stone.restaurant_name).title() + ' flavors list:'.title())
+    cold_stone.print_flavors_list()
+
+if(__name__=='__main__'):
+    main()
--- Ch9_classes/9-8_privileges.py
+++ Ch9_classes/9-8_privileges.py
@@ -3,3 +3,33 @@
 # Move the show_privileges() method to this class. Make a Privileges instance 
 # as an attribute in the Admin class. Create a new instance of Admin and use your 
 # method to show its privileges.
+
+class User():
+    """Simple User Class that creates a user profile"""
+
+    def __init__(self,user_name,first_name,last_name,age):
+        """Initializer to set username, first name, last name, and age"""
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def describe_user(self):
+        """Prints Summary of Users Info"""
+        print(str('username: '+self.user_name).title())
+        print(str('first name: '+self.first_name).title())
+        print(str('last name: '+self.last_name).title())
+        print(str('age: '+self.age).title())
+
+    def greet_user(self):
+        """prints a personalized greeting to user"""
+        print(str('greetings ' + self.user_name + ' thank you for logging in again').title())
+
+def main():
+    """main"""
+    user1 = User('trmart','taylor', 'martin', '27')
+    user1.describe_user()
+    user1.greet_user()
+
+if(__name__=='__main__'):
+    main()
